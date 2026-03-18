bc8014babe102976c5d60f654f1c4a3d556f33bc
HannahZada <hannanu12@gmail.com>
2026-03-19 01:23:26 +0800

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,26 @@
+﻿public class Car : Vehicle
+{
+    private int numPassengers;
+
+    public int NumPassengers
+    {
+        get { return numPassengers; }
+        set { numPassengers = value; }
+    }
+
+    public Car(string make, string model, int year, double dailyRate, int numPassengers)
+        : base(make, model, year, dailyRate)
+    {
+        NumPassengers = numPassengers;
+    }
+
+    public override double CalculateRentalCost(int days)
+    {
+        return DailyRate * days;
+    }
+
+    public override string GetDescription()
+    {
+        return $"{Year} {Make} {Model} ({NumPassengers} passengers) — RM {DailyRate:F2}/day";
+    }
+}
